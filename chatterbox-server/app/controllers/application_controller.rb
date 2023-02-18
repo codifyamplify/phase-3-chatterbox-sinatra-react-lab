9ffaae4aa2365ad3e1f100db8985bfd42f37de4b
David Whitney <codifyamplify@gmail.com>
2023-02-18 11:09:47 -0500

    commit tree 5146cd440423c4451d81a1e5b95b4021a7f8f7cd

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,36 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+#   # Add routes
+#   GET /messages: returns an array of all messages as JSON, ordered by created_at in ascending order.
+  get '/messages' do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+# POST /messages: creates a new message with a body and username from params, and returns the newly created post as JSON.
+  post '/messages' do
+    new_post = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    new_post.to_json
+  end
+# PATCH /messages/:id: updates the body of the message using params, and returns the updated message as JSON.
+  patch '/messages/:id' do
+    # find the message we want to update
+    message = Message.find(params[:id])
+    # update the targeted message
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+# DELETE /messages/:id: deletes the message from the database.
+  delete '/messages/:id' do
+    # find the target
+    message = Message.find(params[:id])
+    # destroy
+    message.destroy
+    message.to_json
+  end
 end
